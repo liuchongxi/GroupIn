62c9987aece7d35a4aedbccaf782d52b83fe3369
tslaven <tslaven@asb9838su-b08.csil.sfu.ca>
2016-11-20 18:30:34 -0800

    more tests

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 20161120031836) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "trainers", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
